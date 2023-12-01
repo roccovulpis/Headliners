6af23791861ac0e6a1120004492e262bb50567a0
ivan-wang-0 <45867687+ivan-wang-0@users.noreply.github.com>
2023-12-01 10:20:22 -0500

    reverse mail.py

--- website/mail.py
+++ website/mail.py
@@ -7,6 +7,7 @@ load_dotenv()
 token = os.getenv('MAILTRAP_TOKEN')
 sender_email = "Headliners@wangify.com"
 
+<<<<<<< HEAD
 def send_email_notification(recipient_email, recipient_name, sender_name, message_content, role,):
 
 	mail = mt.MailFromTemplate(
@@ -20,6 +21,36 @@ def send_email_notification(recipient_email, recipient_name, sender_name, messag
 			"message_content": message_content
  	    }
 	)
+=======
+def send_message(template_id = 5313119, barber_name = "Headliners Barbershop", first_name = "User", message = "default", user_email = None):
+	data = {
+	'Messages': [
+			{
+				"From": {
+					"Email": sender_email,
+					"Name": "Headliners Barbershop"
+				},
+				"To": [
+					{
+						"Email": user_email,
+						"Name": "User"
+					}
+				],
+				"TemplateID": template_id,
+				"TemplateLanguage": True,
+				"Subject": "You have a new message from [[data:barber_name:""]]",
+				"Variables": {
+		"firstname": first_name,
+		"barber_name": barber_name,
+		"message": message
+	}
+			}
+		]
+	}
+	result = mailjet.send.create(data=data)
+	print (result.status_code)
+	print (result.json())
+>>>>>>> parent of 9075323 (cleared db for demo)
 
 	client = mt.MailtrapClient(token=token)
 	client.send(mail)

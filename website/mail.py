a53c269895d8c10d96f4beaac596a1b9f512cf4b
ivan-wang-0 <45867687+ivan-wang-0@users.noreply.github.com>
2023-12-01 10:10:26 -0500

    updated to use mailtrap instead of mailjet created demo accounts

--- website/mail.py
+++ website/mail.py
@@ -1,46 +1,26 @@
-from mailjet_rest import Client
+import mailtrap as mt
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-api_key = os.getenv('MJ_APIKEY_PUBLIC')
-api_secret = os.getenv('MJ_APIKEY_PRIVATE')
-mailjet = Client(auth=(api_key, api_secret), version='v3.1')
-sender_email = "headliners@wangify.com"
+token = os.getenv('MAILTRAP_TOKEN')
+sender_email = "Headliners@wangify.com"
 
-def send_email_notification(recipient_email, recipient_name, sender_name, message_content, role, template_id=5313119):
+def send_email_notification(recipient_email, recipient_name, sender_name, message_content, role,):
 
-	subject = f"You have a new message from {sender_name}"
-	if role == 'barber':
-		subject = (f"New client message from {sender_name}")
-	     
-	data = {
-			'Messages': [
-				{
-					"From": {
-						"Email": sender_email,
-						"Name": "Headliners Barbershop"
-					},
-					"To": [
-						{
-							"Email": recipient_email,
-							"Name": recipient_name
-						}
-					],
-					"TemplateID": template_id,
-					"TemplateLanguage": True,
-					"Subject": subject,
-					"Variables": {
-						"firstname": recipient_name,
-						"barber_name": sender_name,
-						"message": message_content
-					}
-				}
-			]
-		}
-	result = mailjet.send.create(data=data)
-	print(result.status_code)
-	print(result.json())
+	mail = mt.MailFromTemplate(
+		sender=mt.Address(email=sender_email, name="Headliners Barbershop"),
+		to=[mt.Address(email=recipient_email)],
+		template_uuid="af3fdce7-f363-427c-9ba5-b757ef2d79b8",
+  		template_variables={
+			"sender_name": sender_name,
+			"user": recipient_name,
+			"subject": ("New message from ", sender_name),
+			"message_content": message_content
+ 	    }
+	)
 
+	client = mt.MailtrapClient(token=token)
+	client.send(mail)
 
